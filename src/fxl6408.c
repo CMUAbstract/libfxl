94831af2db4f982e9d32e28d9f2d83ef0d0052fc
Alexei Colin <ac@alexeicolin.com>
2017-07-13 22:02:53 -0400

    read id (untested)

---
+++ src/fxl6408.c
@@ -0,0 +1,46 @@
+#include <msp430.h>
+
+#include "fxl6408.h"
+
+#define FXL_ADDR 0x43 // 100 0011
+
+#define FXL_REG_ID 0x01
+
+#define FXL_ID_MFG_MASK 0xC0
+#define FXL_ID_MFG      0x50
+
+
+fxl_status_t fxl_init()
+{
+  UCB0CTLW0 |= UCSWRST; // disable
+  UCB0I2CSA = FXL_ADDR;
+  UCB0CTLW0 &= ~UCSWRST; // enable
+
+  while (UCB0STATW & UCBBUSY);
+
+  UCB0CTLW0 |= UCTR | UCTXSTT; // transmit mode and start
+  while(!(UCB0IFG & UCTXIFG));
+  UCB0TXBUF = FXL_REG_ID;
+  while(!(UCB0IFG & UCTXIFG));
+
+  UCB0CTLW0 &= ~UCTR; // receive mode
+  UCB0CTLW0 |= UCTXSTT; // repeated start
+
+  // wait for addr transmission to finish, data transfer to start
+  while(UCB0CTLW0 & UCTXSTT);
+  UCB0CTLW0 |= UCTXSTP; // stop
+
+  while(!(UCB0IFG & UCRXIFG));
+  uint8_t id = UCB0RXBUF;
+
+  while (UCB0STATW & UCBBUSY);
+
+  if (id & FXL_ID_MFG_MASK != FXL_ID_MFG) {
+    LOG("FXL: invalid MFG id: 0x%02x (expected 0x%02x)\r\n", id & FXL_ID_MFG_MASK, FXL_ID_MFG);
+    return FXL_ERR_BAD_ID;
+  }
+
+  LOG("LSM id: 0x%02x\r\n", id);
+
+  return FXL_SUCCESS;
+}
---
+++ src/include/libfxl/fxl6408.h
@@ -0,0 +1,11 @@
+#ifndef FXL6408_H
+#define FXL6408_H
+
+typedef enum {
+    FXL_SUCCESS = 0
+    FXL_ERR_BAD_ID,
+} fxl_status_t;
+
+fxl_status_t fxl_init();
+
+#endif // FXL6408_H
